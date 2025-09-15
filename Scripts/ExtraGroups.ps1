c5cb5176298151429660ccd232a18b5f23d7c507
Phillip Horn <ph@lts.io>
2025-09-15 10:33:42 -0500

    Refactored scripts with caching, dry-run support, and GUI integration

--- Scripts/ExtraGroups.ps1
+++ Scripts/ExtraGroups.ps1
@@ -1,41 +1,29 @@
-Import-Module ActiveDirectory
-. .\Scripts\Common.ps1
+# ExtraGroups.ps1
+. "$PSScriptRoot\Common.ps1"
+. "$PSScriptRoot\Cache.ps1"
 
-$errorLog = @()
-$extraReport = @()
-$OUs = Get-ADOrganizationalUnit -Filter * -Properties Description
+$results = @()
 
-foreach ($OU in $OUs) {
-    $requiredGroups = Get-InheritedGroups -OU $OU.DistinguishedName
-    try {
-        $users = Get-ADUser -Filter * -SearchBase $OU.DistinguishedName -SearchScope OneLevel -Properties MemberOf
-    } catch {
-        Log-Error -Context "Get-ADUser" -Target $OU.DistinguishedName -Details $_.Exception.Message -ErrorLog ([ref]$errorLog)
-        continue
+foreach ($user in $UserCache) {
+    $userDN = $user.DistinguishedName
+    $userGroups = $user.MemberOf | ForEach-Object {
+        if ($GroupCache.ContainsKey($_)) {
+            $GroupCache[$_].Name
+        }
     }
 
-    foreach ($user in $users) {
-        try {
-            $userGroups = ($user.MemberOf | ForEach-Object {
-                (Get-ADGroup -Identity $_).Name
-            }) | Sort-Object -Unique
-        } catch {
-            Log-Error -Context "Get-ADGroup" -Target $user.SamAccountName -Details $_.Exception.Message -ErrorLog ([ref]$errorLog)
-            continue
-        }
+    $ou = ($OUCache[$userDN] -as [Microsoft.ActiveDirectory.Management.ADOrganizationalUnit])
+    $requiredGroups = Get-InheritedGroups $ou
 
-        $extraGroups = $userGroups | Where-Object { $_ -notin $requiredGroups }
+    $extra = $userGroups | Where-Object { $_ -notin $requiredGroups }
 
-        if ($extraGroups.Count -gt 0) {
-            $extraReport += [PSCustomObject]@{
-                Username     = $user.SamAccountName
-                OU           = $OU.DistinguishedName
-                ExtraGroups  = ($extraGroups -join ', ')
-            }
+    if ($extra.Count -gt 0) {
+        $results += [PSCustomObject]@{
+            User          = $user.SamAccountName
+            OU            = $ou.Name
+            ExtraGroups   = ($extra -join ", ")
         }
     }
 }
 
-$extraReport | Export-Csv ".\Reports\AD_ExtraGroups_Report.csv" -NoTypeInformation
-$timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
-$errorLog | Export-Csv ".\Logs\AD_ErrorLog_$timestamp.csv" -NoTypeInformation
+$results | Export-Csv -Path ".\Reports\AD_ExtraGroups_Report.csv" -NoTypeInformation
